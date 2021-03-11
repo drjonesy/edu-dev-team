690a7edaaaf4e10dc20ad25609b563965ed2e7a4
drjonesy <rjermanjones@gmail.com>
2021-03-10 21:57:51 -0600

    course tile cards will auto generate correctly now

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@
 // Could not use pure pure JSON. Had to prepend: 'const courses =' 
 // This files assumes courses.js is imported before this script
 
-const cardTile = (obj) => {
+function cardTile(obj){
     return `
     <div class="col-sm">
     <div class="card" style="width: 18rem;">
@@ -10,35 +10,27 @@ const cardTile = (obj) => {
     <div class="card-body">
     <h5 class="card-title">${obj.title}</h5>
     <p class="card-text">${obj.desc}</p>
-    <a href="${obj.courseURL}" class="btn btn-primary">${obj.courseURL}</a>
-    </div>
+    <a href="${obj.courseURL}" class="btn btn-primary">View Micro-Course</a>
     </div>
     </div>`;
 }
 
-const addCards = (id, ...items) => {
-    let row = '<div class="row">'
-    for (const item of items) {
-        row += item;
-        row += " ";
-    }
-    row += '</div>';
-    return row;
-}
-
-const addRow = (element, html) => {
-    const div = document.querySelector(element);
-    div.innerHTML = html;
-    
-}
-
-// MAIN 
-let x = addCards("results", cardTile(courses.git), cardTile(courses.git));
-addRow("#content", x);
 
 // iterate over each course
 // create new row
 // if n number of courses iterated create new row
+const body = document.querySelector('body');
+const blockCards = document.createElement("div");
+blockCards.id = "blockCards";
+blockCards.className = "container";
+const cardRow = document.createElement("div");
+cardRow.className = "row";
 
+// add elements
+body.prepend(blockCards);
+blockCards.append(cardRow);
 
+for(const obj in courses){
+    cardRow.innerHTML += cardTile(courses[obj]);
+}
 
--- src/js/courses.js
+++ src/js/courses.js
@@ -6,11 +6,18 @@ const courses = {
         "image": "src/images/git.png",
         "courseURL": "git-version-control/README.md"
     },
-    "code.org": {
+    "codeorg": {
         "keywords": ["programming", "code", "stem"],
         "title": "Intro to Coding",
         "desc": "A free and fun way to learning computer science.",
-        "image": "src/images/git.png",
+        "image": "src/images/code-org.png",
         "courseURL": "code-org/README.md"
+    },
+    "tinkercad": {
+        "keywords": ["cad", "3d", "stem", "model", "design", "3d printing", "electronics"],
+        "title": "Start Tinkering in 3D",
+        "desc": "Bring your 3D thoughts to life using this free simple CAD tool.",
+        "image": "src/images/tinkercad.png",
+        "courseURL": "tinkercad/README.md"
     }
 }
