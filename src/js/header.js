a7793745cb9eeefd9345386a59800707bb0589b3
drjonesy <rjermanjones@gmail.com>
2021-04-23 11:27:12 -0500

    [STYLE CHANGE] Fixed alignment issues with menu buttons. Desktop=Right, Mobile or Tablet=Left

--- src/js/header.js
+++ src/js/header.js
@@ -6,7 +6,7 @@ const header = (linkHome, linkAbout) => {
       <a class="navbar-brand" href="${linkHome}"><h1>EduDevTeam</h1></a>
     </div>
     <div class="logo-border mt-1 mb-3"></div>
-    <div class="col-md float-right">
+    <div class="col-md text-right">
       <a href="${linkHome}"><button class="rounded-btn bg-pink text-white nav-margin btn-hover">home</button></a>  
       <a href="${linkAbout}"><button class="rounded-btn bg-violet text-white nav-margin btn-hover">about</button></a>
     </div>
